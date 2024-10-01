e918a480d98eba32b65516367552bfd2ff018190
Nassredean Nasseri <nassredean@gmail.com>
2024-09-30 22:05:36 -0400

    Remove starship for simple fish prompt

--- fish/config.fish
+++ fish/config.fish
@@ -27,4 +27,49 @@ function fish_erase_path
   set --erase --universal fish_user_paths[$argv[1]]
 end
 
-starship init fish | source
+set __fish_git_prompt_showdirtystate 'yes'
+set __fish_git_prompt_showstashstate 'yes'
+set __fish_git_prompt_showuntrackedfiles 'yes'
+set __fish_git_prompt_showupstream 'yes'
+set __fish_git_prompt_color_branch yellow
+set __fish_git_prompt_color_upstream_ahead green
+set __fish_git_prompt_color_upstream_behind red
+set __fish_git_prompt_color_prefix white
+set __fish_git_prompt_color_suffix white
+
+function fish_prompt -d "Prints left prompt"
+  set -l glyph        "=>"
+  set -l glyph_color  (set_color brcyan)
+  set -l pwd          (basename (prompt_pwd))
+  set -l pwd_color    (set_color bryellow)
+  set -l normal_color (set_color normal)
+  set -l venv_color   (set_color blue)
+
+  if test (id -u "$USER") -eq 0
+      set glyph "#"
+  end
+
+  printf "$pwd_color$pwd"
+  printf "%s\n" (__fish_git_prompt)
+  if test -n "$VIRTUAL_ENV"
+      set -l virtualenv (basename "$VIRTUAL_ENV")
+      printf "$normal_color($venv_color$virtualenv$normal_color) "
+  end
+  printf "$glyph_color$glyph $normal_color"
+end
+
+function fish_right_prompt -d "Prints right prompt"
+  set -l last_status  $status
+  set -l stat         "✔️"
+  if test "$last_status" -ne 0
+      set stat "❌"
+  end
+
+  if test $CMD_DURATION
+      set duration (echo "$CMD_DURATION 1000" | awk '{printf "%.3fs", $1 / $2}')
+      printf $duration
+  end
+
+  printf " $stat"
+end
+
