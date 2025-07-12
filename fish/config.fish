4c133a3050aa37103774c68f50ca0fc6dfe80835
Nassredean Nasseri <nassredean@gmail.com>
2025-07-12 13:29:03 -0400

    Update fish prompt

--- fish/config.fish
+++ fish/config.fish
@@ -8,7 +8,7 @@ fish_add_path /Users/dean/.local/bin
 fish_add_path /usr/local/bin
 
 function cat --wraps bat
-  bat $argv
+  bat -p $argv
 end
 
 function vim --wraps nvim
@@ -23,30 +23,54 @@ function fish_remove_path
   set --erase --universal fish_user_paths[$argv[1]]
 end
 
-set __fish_git_prompt_showdirtystate 'yes'
-set __fish_git_prompt_showstashstate 'yes'
-set __fish_git_prompt_showuntrackedfiles 'yes'
-set __fish_git_prompt_showupstream 'yes'
-set __fish_git_prompt_color_branch yellow
-set __fish_git_prompt_color_upstream_ahead green
-set __fish_git_prompt_color_upstream_behind red
-set __fish_git_prompt_color_prefix white
-set __fish_git_prompt_color_suffix white
+### Git-prompt settings
+set -U __fish_git_prompt_showdirtystate      yes
+set -U __fish_git_prompt_showstashstate      yes
+set -U __fish_git_prompt_showuntrackedfiles  yes
+set -U __fish_git_prompt_showupstream        yes
+set -U __fish_git_prompt_color_branch        yellow
+set -U __fish_git_prompt_color_upstream_ahead   green
+set -U __fish_git_prompt_color_upstream_behind  red
+set -U __fish_git_prompt_color_prefix        white
+set -U __fish_git_prompt_color_suffix        white
 
-function fish_prompt -d "Prints left prompt"
-  set -l glyph        "=>"
-  set -l glyph_color  (set_color brcyan)
-  set -l pwd          (basename (prompt_pwd))
-  set -l pwd_color    (set_color bryellow)
-  set -l normal_color (set_color normal)
-  set -l venv_color   (set_color blue)
+### Suppress Fish’s default vi-mode banner (avoids the extra blank line)
+function fish_mode_prompt; end
 
-  if test (id -u "$USER") -eq 0
-      set glyph "#"
-  end
+### Suppress right prompt
+function fish_right_prompt; end
 
-  printf "$pwd"
-  printf "%s\n" (__fish_git_prompt)
-  printf "$glyph "
+function fish_prompt --description 'Compact prompt with hh:mm:ss & git status'
+    set -l last_status $status
+
+    # colours
+    set -l yellow (set_color bryellow)
+    set -l cyan   (set_color brcyan)
+    set -l green  (set_color brgreen)
+    set -l red    (set_color brred)
+    set -l normal (set_color normal)
+
+    # pieces
+    set -l ts  (date '+%H:%M:%S')          # hh:mm:ss
+    set -l dir (prompt_pwd --dir-length=1)  # ~/w/dotfiles or ~/p/w/api, etc.
+
+    set -l git (__fish_git_prompt)
+    if test -n "$git"
+        set git " $git"                    # add one leading space only if inside a repo
+    end
+
+    set -l glyph (test (id -u) -eq 0; and echo '#'; or echo '➜')
+    set -l exit  (test $last_status -eq 0; and echo "$green✔$normal"; or echo "$red✗$normal")
+
+    # render – no stray spaces or newlines
+    printf '%s[%s] %s%s%s%s %s %s ' \
+        $yellow $ts \
+        $cyan $dir $normal \
+        $git \
+        $exit \
+        $glyph
 end
+
+
+fzf --fish | source
 
